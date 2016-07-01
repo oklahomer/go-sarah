24055c8bb9b9d902d38e7d67ee42097b3ceced28
Oklahomer <hagiwara.go@gmail.com>
2016-07-01 23:18:10 +0900

    Some tests on retry

--- retry/retry_test.go
+++ retry/retry_test.go
@@ -18,3 +18,27 @@ func TestRetry(t *testing.T) {
 		t.Errorf("something is wrong with retrial. %s.", err.Error())
 	}
 }
+
+func TestSomeRetrial(t *testing.T) {
+	trial := uint(3)
+	i := uint(0)
+	expectedStr := "abc"
+	str := ""
+	err := Retry(trial, func() error {
+		i++
+		if i >= trial {
+			return errors.New(fmt.Sprintf("error on %d", i))
+		}
+
+		str = expectedStr
+		return nil
+	})
+
+	if err != nil {
+		t.Errorf("error is returned where it was not expected. %s.", err.Error())
+	}
+
+	if str != expectedStr {
+		t.Errorf("expected string is not returned. instead %s is returned.", str)
+	}
+}
