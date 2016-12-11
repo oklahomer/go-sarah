8566cadd3e471c3bb66bb76a1b8194568b3c66d3
Oklahomer <hagiwara.go@gmail.com>
2016-12-11 15:06:08 +0900

    Adapters' configs are now JSON/YAML unmarshalable

--- gitter/config.go
+++ gitter/config.go
@@ -3,15 +3,17 @@ package gitter
 import "time"
 
 type Config struct {
-	Token         string
-	RetryLimit    uint
-	RetryInterval time.Duration
+	Token         string        `json:"token" yaml:"token"`
+	RetryLimit    uint          `json:"retry_limit" yaml:"retry_limit"`
+	RetryInterval time.Duration `json:"retry_interval" yaml:"retry_interval"`
 }
 
-func NewConfig(token string) *Config {
-	// minimal configuration
+// NewConfig returns initialized Config struct with default settings.
+// Token is empty at this point. Token can be set by feeding this instance to json.Unmarshal/yaml.Unmarshal,
+// or direct assignment.
+func NewConfig() *Config {
 	return &Config{
-		Token:         token,
+		Token:         "",
 		RetryLimit:    10,
 		RetryInterval: 500 * time.Millisecond,
 	}
--- slack/config.go
+++ slack/config.go
@@ -3,17 +3,19 @@ package slack
 import "time"
 
 type Config struct {
-	Token            string
-	SendingQueueSize uint
-	RetryLimit       uint
-	RetryInterval    time.Duration
-	PingInterval     time.Duration
+	Token            string        `json:"token" yaml:"token"`
+	SendingQueueSize uint          `json:"sending_queue_size" yaml:"sending_queue_size"`
+	RetryLimit       uint          `json:"retry_limit" yaml:"retry_limit"`
+	RetryInterval    time.Duration `json:"retry_interval" yaml:"retry_interval"`
+	PingInterval     time.Duration `json:"ping_interval" yaml:"ping_interval"`
 }
 
-func NewConfig(token string) *Config {
-	// minimal configuration
+// NewConfig returns initialized Config struct with default settings.
+// Token is empty at this point. Token can be set by feeding this instance to json.Unmarshal/yaml.Unmarshal,
+// or direct assignment.
+func NewConfig() *Config {
 	return &Config{
-		Token:            token,
+		Token:            "",
 		SendingQueueSize: 100,
 		RetryLimit:       10,
 		RetryInterval:    500 * time.Millisecond,
