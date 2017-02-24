bfaefc4862edfe50e4540e0022f13e6e2bbb76d3
Oklahomer <hagiwara.go@gmail.com>
2017-02-24 23:33:23 +0900

    Fix comment

--- plugins/echo/echo.go
+++ plugins/echo/echo.go
@@ -22,7 +22,7 @@ func SlackCommandFunc(_ context.Context, input sarah.Input) (*sarah.CommandRespo
 	return slack.NewStringResponse(commandFnc(input)), nil
 }
 
-// SlackCommandFunc is a sarah.CommandFunc especially designed for gitter adapter.
+// GitterCommandFunc is a sarah.CommandFunc especially designed for gitter adapter.
 func GitterCommandFunc(_ context.Context, input sarah.Input) (*sarah.CommandResponse, error) {
 	return gitter.NewStringResponse(commandFnc(input)), nil
 }
