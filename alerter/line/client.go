09c88c8eb8c8828fca1c0b8de5da83c65d47a813
Oklahomer <hagiwara.go@gmail.com>
2017-04-11 22:43:50 +0900

    Add comments to exported values/methods.

--- alerter/line/client.go
+++ alerter/line/client.go
@@ -11,13 +11,18 @@ import (
 	"time"
 )
 
+// Endpoint defines the API endpoint to be used for notification.
 var Endpoint = "https://notify-api.line.me/api/notify"
 
+// Config contains some configuration variables for gitter Adapter.
 type Config struct {
 	Token          string        `json:"token" yaml:"token"`
 	RequestTimeout time.Duration `json:"timeout" yaml:"timeout"`
 }
 
+// NewConfig returns initialized Config struct with default settings.
+// Token is empty at this point. Token can be set by feeding this instance to json.Unmarshal/yaml.Unmarshal,
+// or direct assignment.
 func NewConfig() *Config {
 	return &Config{
 		Token:          "", // Updated on json/yaml unmarshal or by manually
@@ -25,16 +30,19 @@ func NewConfig() *Config {
 	}
 }
 
+// Client is an API client for LINE notification.
 type Client struct {
 	config *Config
 }
 
+// New creates and returns new Client instant.
 func New(config *Config) *Client {
 	return &Client{
 		config: config,
 	}
 }
 
+// Alert sends alert message to notify critical state of caller.
 func (c *Client) Alert(ctx context.Context, botType sarah.BotType, err error) error {
 	reqCtx, cancel := context.WithTimeout(ctx, c.config.RequestTimeout)
 	defer cancel()
